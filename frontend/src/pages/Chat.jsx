76fe79806fc2a33b2babe8c84277e1b3234a7885
ritikjoshi22 <joshster.ritik@gmail.com>
2024-05-31 18:15:01 +0545

    chat header and logout

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -5,17 +5,20 @@ import { useNavigate } from "react-router-dom";
 import { allUsersRoute } from "../utils/APIRoutes";
 import Contacts from "../components/Contacts";
 import Welcome from "../components/Welcome";
+import { ChatContainer } from "../components/ChatContainer";
 const Chat = () => {
   const navigate = useNavigate();
   const [contacts, setContacts] = useState([]);
   const [currentUser, setCurrentUser] = useState(undefined);
   const [currentChat, setCurrentChat] = useState(undefined);
+  const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
     const fetchUser = async () => {
       if (!localStorage.getItem("chat-app-user")) {
         navigate("/login");
       } else {
-        setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")))
+        setCurrentUser(await JSON.parse(localStorage.getItem("chat-app-user")));
+        setIsLoaded(true);
       } 
     }
     fetchUser();
@@ -45,7 +48,11 @@ const Chat = () => {
     <Container>
       <div className="container">
         <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange} />
-        <Welcome currentUser={currentUser} />
+        {
+          isLoaded && currentChat === undefined ? (<Welcome currentUser={currentUser} />) : (
+            <ChatContainer currentChat={currentChat} />
+          )
+        }
       </div>
     </Container>
   );
